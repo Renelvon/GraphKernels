e99fb84586055b190b77bfa3af18d8add56a0efa
Nikolaos Korasidis <renelvon@gmail.com>
2019-10-08 12:20:33 +0200

    Improve setup script
    * Convert tabs to spaces and reindent file.
    * Package setup as main function
    * Point package URL to group GitHub.
    * Automatically source package version; see
      https://packaging.python.org/guides/single-sourcing-package-version/

--- src/graphkernels/setup.py
+++ src/graphkernels/setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
-setup(name = 'graphkernels',
-      version = '0.2.1',
-	description = 'Package for computing graph kernels',
-	url = 'https://github.com/eghisu/GraphKernels/tree/master/graphkernels',
-	author = 'Elisabetta Ghisu',
-	author_email = 'elisabetta.ghisu@bsse.ethz.ch',
-	license = 'ETH Zurich',
-	packages = ['graphkernels'],
+import graphkernels
 
-	install_requires = ['GKextCPy','python-igraph', 'numpy'],
-	package_data={'graphkernels': ['data.mutag']},
-	) # maybe to check github url
+
+def main():
+    setup(
+        name='graphkernels',
+        version=graphkernels.__version__,
+        description='Package for computing graph kernels',
+        url='https://github.com/BorgwardtLab/GraphKernels',
+        author='Elisabetta Ghisu',
+        author_email='elisabetta.ghisu@bsse.ethz.ch',
+        license='ETH Zurich',
+        packages=['graphkernels'],
+        install_requires=['GKextCPy', 'python-igraph', 'numpy'],
+        package_data={'graphkernels': ['data.mutag']},
+    )
+
+
+if __name__ == '__main__':
+    main()
