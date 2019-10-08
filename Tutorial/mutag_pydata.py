ea55ca4f9885b8fb9505b0d6be48f0513dccb1eb
Nikolaos Korasidis <renelvon@gmail.com>
2019-10-08 16:12:35 +0200

    Refactor MUTAG script
    * Use list comprehension and drop xrange.
    * Pack into main function.

--- Tutorial/mutag_pydata.py
+++ Tutorial/mutag_pydata.py
@@ -1,18 +1,23 @@
-
 """
 Converting mutag to list format
 """
 
-from igraph import Graph
 import numpy as np
+from igraph import Graph
 
-mutag_list = []
+N_GRAPHS = 188
 
-n_graphs = 188
-for i in xrange(n_graphs):
+def main():
+    mutag_list = [
+        Graph.Read_GraphML(
+            "/home/eghisu/projects/gk_python_wrapper/gk_python_c/data/mutag/mutag_%d.graphml" % i
+        ) for i in range(1, N_GRAPHS + 1)
+    ]
+    np.save(
+        "/home/eghisu/projects/gk_python_wrapper/gk_python_c/data/mutag_pydata",
+        mutag_list
+    )
 
-	g_cur = Graph.Read_GraphML("/home/eghisu/projects/gk_python_wrapper/gk_python_c/data/mutag/mutag_%d.graphml" % (i+1))
-	mutag_list.append(g_cur)
 
-
-np.save("/home/eghisu/projects/gk_python_wrapper/gk_python_c/data/mutag_pydata", mutag_list)
+if __name__ == '__main__':
+    main()
