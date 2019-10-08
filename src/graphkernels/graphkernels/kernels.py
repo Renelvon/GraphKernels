8745d56d4ceb42814a048ddffe9033fa9ca32f90
Nikolaos Korasidis <renelvon@gmail.com>
2019-10-08 16:34:36 +0200

    Factor computation of common kernels
    * Section kernels using comments.
    * Fix module docstring.

--- src/graphkernels/graphkernels/kernels.py
+++ src/graphkernels/graphkernels/kernels.py
@@ -1,6 +1,5 @@
 """
-Collect all the functions
-for computing the graph kernels
+Functions for computing the graph kernels
 """
 
 import numpy as np
@@ -11,118 +10,94 @@ import GKextCPy as gkCpy
 from .utilities import GetGKInput, GetAdjMatList
 
 
-def CalculateEdgeHistKernel(G, par=-1.0):
-    """Edge Histogram Kernel"""
+def _do_calculate(G, gk_par, kernel_id=None):
+    if not isinstance(kernel_id, int):
+        raise TypeError('Kernel index must be integer')
 
-    # Extract graph info
+    if kernel_id <= 0:
+        raise ValueError('Kernel index must be positive')
+
+    # Extract graph info.
     E, V_label, V_count, E_count, D_max = GetGKInput(G)
 
-    par = gkCpy.DoubleVector([par])
+    # Compute designated kernel
+    return gkCpy.CalculateKernelPy(
+        E, V_label, V_count, E_count, D_max,
+        gk_par,
+        kernel_id
+    )
 
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 1)
+
+# === Linear Kernels on Histograms ===
+
+def CalculateEdgeHistKernel(G, par=-1.0):
+    """Edge Histogram Kernel"""
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 1)
 
 
 def CalculateVertexHistKernel(G, par=-1.0):
     """Vertex Histogram Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 2)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 2)
 
 
 def CalculateVertexEdgeHistKernel(G, par=-1.0):
     """Vertex Edge Histogram Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 3)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 3)
 
 
 def CalculateVertexVertexEdgeHistKernel(G, par=1):
     """Vertex Vertex Edge Histogram Kernel"""
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 4)
 
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
 
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 4)
-
+# === RBF Kernels on Histograms ===
 
 def CalculateEdgeHistGaussKernel(G, par=1):
     """Edge Histogram RBF Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 5)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 5)
 
 
 def CalculateVertexHistGaussKernel(G, par=1):
     """Vertex Histogram RBF Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 6)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 6)
 
 
 def CalculateVertexEdgeHistGaussKernel(G, par=1):
     """Vertex Edge Histogram RBF Kernel"""
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 7)
 
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
 
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 7)
-
+# === Random Walk Kernels ===
 
 def CalculateGeometricRandomWalkKernel(G, par=1):
     """Geometric Random Walk Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 8)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 8)
 
 
 def CalculateExponentialRandomWalkKernel(G, par=1):
     """Exponential Random Walk Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
-    par = gkCpy.DoubleVector([par])
-
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 9)
+    gk_par = gkCpy.DoubleVector([par])
+    return _do_calculate(G, gk_par, 9)
 
 
 def CalculateKStepRandomWalkKernel(G, par=1):
     """K-step Random Walk Kernel"""
-
-    # Extract graph info
-    E, V_label, V_count, E_count, D_max = GetGKInput(G)
-
+    # Allow user to provide own list of k-step weights
     if isinstance(par, (int, float, complex)):
-        par = gkCpy.DoubleVector([par])
+        par = [par]
+    gk_par = gkCpy.DoubleVector(par)
+    return _do_calculate(G, gk_par, 10)
 
-    else:
-        par = gkCpy.DoubleVector(par)
 
-    return gkCpy.CalculateKernelPy(E, V_label, V_count, E_count, D_max, par, 10)
-
+# === Advanced Kernels ===
 
 def CalculateWLKernel(G, par=5):
     """Weisfeiler-Lehman Kernel Kernel"""
