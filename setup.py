f66e8e54ff6342afe4ea045bcf02d7fc54454232
Nikolaos Korasidis <nkorasid@student.ethz.ch>
2019-10-24 21:01:27 +0200

    Remove duplicate options from setup script

--- setup.py
+++ setup.py
@@ -13,10 +13,8 @@ import setuptools
 
 import graphkernels
 
-README = 'README.md'
 THIS_DIR = path.dirname(__file__)
 GK_DIR = path.join(THIS_DIR, 'graphkernels')
-README_PATH = path.join(THIS_DIR, README)
 
 
 def get_eigen_include_dir():
@@ -37,23 +35,8 @@ def get_eigen_include_dir():
 
 
 def main():
-    with open(README_PATH, 'r') as f_readme:
-        long_description = f_readme.read()
-
+    # Rest of options are specified in `setup.cfg`
     setuptools.setup(
-        author='Elisabetta Ghisu',
-        author_email='elisabetta.ghisu@bsse.ethz.ch',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Programming Language :: C++',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Topic :: Scientific/Engineering :: Mathematics',
-        ],
-        description='Package for computing graph kernels',
         ext_modules=[
             setuptools.Extension(
                 '_graphkernels',
@@ -72,28 +55,10 @@ def main():
                 ]
             )
         ],
-        include_package_data=True,
-        install_requires=[
-            'numpy>=1.11',
-            'pkgconfig',
-            'python-igraph'
-        ],
-        license='ETH Zurich',
-        long_description=long_description,
         # NOTE: The following option may produce a harmless warning when
         # building package using setuptools < 40.*
         long_description_content_type='text/markdown',
-        name='graphkernels',
-        packages=['graphkernels'],
-        python_requires='>=3.4',
-        setup_requires=[
-            'numpy>=1.11',
-            'pkgconfig'
-        ],
-        tests_require=['cpplint', 'pylint'],
-        url='https://github.com/BorgwardtLab/GraphKernels',
         version=graphkernels.__version__,
-        zip_safe=False
     )
 
 
