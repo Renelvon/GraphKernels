dd44518bab5f3f4fa5dcb683fdcf07803451429f
Nikolaos Korasidis <nkorasid@student.ethz.ch>
2019-10-30 10:32:27 +0100

    Do not export kernel-specific auxiliary functions

--- graphkernels/cppkernels/rest.h
+++ graphkernels/cppkernels/rest.h
@@ -12,31 +12,6 @@
 // Random Walk kernels
 // ===================
 
-double geometricRandomWalkKernel(Eigen::MatrixXi& e1,
-                                 Eigen::MatrixXi& e2,
-                                 std::vector<int>& v1_label,
-                                 std::vector<int>& v2_label,
-                                 double lambda);
-
-double exponentialRandomWalkKernel(Eigen::MatrixXi& e1,
-                                   Eigen::MatrixXi& e2,
-                                   std::vector<int>& v1_label,
-                                   std::vector<int>& v2_label,
-                                   double beta);
-
-double kstepRandomWalkKernel(Eigen::MatrixXi& e1,
-                             Eigen::MatrixXi& e2,
-                             std::vector<int>& v1_label,
-                             std::vector<int>& v2_label,
-                             std::vector<double>& lambda_list);
-
-double computeKernelValue(Eigen::MatrixXi& e1,
-                          Eigen::MatrixXi& e2,
-                          std::vector<int>& v1_label,
-                          std::vector<int>& v2_label,
-                          std::vector<double>& par,
-                          int kernel_type);
-
 Eigen::MatrixXd CalculateKernelPy(std::vector<Eigen::MatrixXi>& E,
                                   std::vector<std::vector<int>>& V_label,
                                   std::vector<double>& par,
