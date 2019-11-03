b155deef1b744432bf1c0a09bc9e556d0a40f96b
Nikolaos Korasidis <nkorasid@student.ethz.ch>
2019-11-03 17:29:34 +0100

    Format file and delete dead code

--- graphkernels/cppkernels/connected_graphlet.h
+++ graphkernels/cppkernels/connected_graphlet.h
@@ -12,8 +12,8 @@
 // Connected Graphlet kernel for k = 3, 4, 5
 // -----------------------------------------
 Eigen::MatrixXd CalculateConnectedGraphletKernelPy(
-    std::vector<Eigen::MatrixXi>& graph_adj_all,
-    std::vector<std::vector<std::vector<int>>>& graph_adjlist_all,
-    int k);
+        std::vector<Eigen::MatrixXi>& graph_adj_all,
+        std::vector<std::vector<std::vector<int>>>& graph_adjlist_all,
+        int k);
 
 #endif  // GRAPHKERNELS_CPPKERNELS_CONNECTED_GRAPHLET_H_
